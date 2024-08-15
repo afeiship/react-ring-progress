ad0bf765c04ff65f54115fd677e1bfb409145dd9
afeiship <1290657123@qq.com>
2024-08-15 23:20:07 +0800

    chore: upt children impl

--- packages/example/src/app.tsx
+++ packages/example/src/app.tsx
@@ -2,13 +2,6 @@ import ReactRingProgress from '@jswork/react-ring-progress/src/main';
 import '@jswork/react-ring-progress/src/style.scss';
 
 function App() {
-  const childFn = ({ value }) => {
-    return <div className="absolute text-center layout-trans-center-xy y">
-      <p>竞争力</p>
-      <h2 className="f-10">{value}%</h2>
-    </div>;
-  };
-
   return (
     <div className="m-10 p-4 shadow bg-gray-100 text-gray-800 hover:shadow-md transition-all">
       <div className="badge badge-warning absolute right-0 top-0 m-4">
@@ -19,8 +12,12 @@ function App() {
           lineCap="round"
           stroke={{ start: '#007aff', end: '#ff5722' }}
           value={80}
-          lineWidth={10}
-          children={childFn} />
+          lineWidth={10}>
+          <div className="absolute text-center layout-trans-center-xy y">
+            <p>竞争力</p>
+            <h2 className="f-10">80%</h2>
+          </div>
+        </ReactRingProgress>
       </div>
     </div>
   );
